4253980decb1db03a9e00e5c184fb762de7f9018
proHyundo <guseh08@knou.ac.kr>
2024-02-11 16:52:06 +0900

    test(HealthRepositoryTest): Add test to check connection of the H2 in-memory database on testing environment

---
+++ src/test/java/com/swyp3/babpool/infra/health/dao/HealthRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.swyp3.babpool.infra.health.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@MybatisTest
+class HealthRepositoryTest {
+
+    @Autowired
+    HealthRepository healthRepository;
+
+    @DisplayName("DB 초기화 후, 전체 행 개수를 조회하면 3이어야 한다.")
+    @Test
+    void countAllResult3AfterInit() {
+        // when
+        Integer resultInt = healthRepository.countAll();
+        // then
+        assertThat(resultInt).isEqualTo(3);
+    }
+
+}
