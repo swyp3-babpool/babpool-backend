c23f7073aa03cce6249b92cce2a618a3875d0f4b
kang yeon joo <rkdduswndi@naver.com>
2024-02-28 23:20:37 +0900

    fix(auth): change api

--- src/main/java/com/swyp3/babpool/infra/auth/controller/AuthController.java
+++ src/main/java/com/swyp3/babpool/infra/auth/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api/user")
 public class AuthController {
     private final AuthService authService;
 
