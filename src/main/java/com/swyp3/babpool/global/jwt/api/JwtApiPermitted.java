50ac065527d0b3efdeebd9064f4ae2cd52d50ebd
proHyundo <guseh08@knou.ac.kr>
2024-03-01 20:46:02 +0900

    feat(jwt): Add refresh access token request api

---
+++ src/main/java/com/swyp3/babpool/global/jwt/api/JwtApiPermitted.java
@@ -0,0 +1,33 @@
+package com.swyp3.babpool.global.jwt.api;
+
+import com.swyp3.babpool.global.common.response.ApiResponse;
+import com.swyp3.babpool.global.jwt.application.JwtService;
+import com.swyp3.babpool.global.jwt.exception.BabpoolJwtException;
+import com.swyp3.babpool.global.jwt.exception.errorcode.JwtExceptionErrorCode;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class JwtApiPermitted {
+
+    private final JwtService jwtService;
+
+    @PostMapping("/api/token/access/refresh")
+    public ApiResponse<String> refreshAccessToken(HttpServletRequest request){
+        Optional<Cookie> refreshTokenCookie = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals("refreshToken"))
+                .findFirst();
+        String refreshToken = refreshTokenCookie.map(Cookie::getValue).orElseThrow(
+                () -> new BabpoolJwtException(JwtExceptionErrorCode.NOT_FOUND_TOKEN, "RefreshToken 이 존재하지 않습니다."));
+        return ApiResponse.of(HttpStatus.OK, "로그인 연장 성공", jwtService.extendLoginState(refreshToken));
+    }
+
+}
