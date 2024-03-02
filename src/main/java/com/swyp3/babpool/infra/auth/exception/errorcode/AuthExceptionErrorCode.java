086d0aae1e339f058954a33b7933f421d33ec479
kang yeon joo <rkdduswndi@naver.com>
2024-03-03 00:15:29 +0900

    fix(auth): Add ErrorCode AUTH_ERROR_CONNECT_WITH_KAKAO

--- src/main/java/com/swyp3/babpool/infra/auth/exception/errorcode/AuthExceptionErrorCode.java
+++ src/main/java/com/swyp3/babpool/infra/auth/exception/errorcode/AuthExceptionErrorCode.java
@@ -12,7 +12,8 @@ public enum AuthExceptionErrorCode implements CustomErrorCode {
     AUTH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST,"유효하지 않은 토큰입니다."),
     AUTH_MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED,"올바르지 않은 토큰입니다."),
     AUTH_UNSUPPORTED_ID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED,"OAuth Identity Token의 형식이 올바르지 않습니다."),
-    AUTH_PUBLIC_KEY_ERROR(HttpStatus.UNAUTHORIZED,"OAuth Public Key 생성 중 오류가 발생했습니다.");
+    AUTH_PUBLIC_KEY_ERROR(HttpStatus.UNAUTHORIZED,"OAuth Public Key 생성 중 오류가 발생했습니다."),
+    AUTH_ERROR_CONNECT_WITH_KAKAO(HttpStatus.INTERNAL_SERVER_ERROR,"Kakao로부터 id Token을 응답받는 과정 중 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
