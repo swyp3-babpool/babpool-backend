983ffddd02c6089c5818f5f437fe3d6bfa765d4e
proHyundo <guseh08@knou.ac.kr>
2024-03-06 14:41:36 +0900

    refactor(profile): Update profile api get mapping url from 'card' to 'update'

--- src/main/java/com/swyp3/babpool/domain/profile/api/ProfileApi.java
+++ src/main/java/com/swyp3/babpool/domain/profile/api/ProfileApi.java
@@ -2,6 +2,7 @@ package com.swyp3.babpool.domain.profile.api;
 
 import com.swyp3.babpool.domain.profile.api.request.ProfileUpdateRequest;
 import com.swyp3.babpool.domain.profile.application.ProfileService;
+import com.swyp3.babpool.domain.profile.application.response.ProfileDetailResponse;
 import com.swyp3.babpool.domain.profile.application.response.ProfileUpdateResponse;
 import com.swyp3.babpool.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/profile")
 public class ProfileApi {
-
     private final ProfileService profileService;
 
-    @PostMapping("/api/profile/card")
+    @PostMapping("/update")
     public ApiResponse<ProfileUpdateResponse> updateProfileCard(@RequestAttribute(value = "userId") Long userId,
                                                                 @RequestPart(value = "profileImageFile") MultipartFile multipartFile,
                                                                 @RequestPart(value = "profileInfo") ProfileUpdateRequest profileUpdateRequest) {
@@ -22,5 +23,12 @@ public class ProfileApi {
         ProfileUpdateResponse profileResponse = profileService.saveProfileInfo(userId, profileUpdateRequest);
         return ApiResponse.ok(profileResponse);
     }
+
+    @GetMapping("/detail/{targetProfileId}")
+    public ApiResponse<ProfileDetailResponse> getProfileDetail(@PathVariable(name="targetProfileId") Long targetProfileId){
+        ProfileDetailResponse profileDetailResponse = profileService.getProfileDetail(targetProfileId);
+        return ApiResponse.ok(profileDetailResponse);
+    }
+
 
 }
