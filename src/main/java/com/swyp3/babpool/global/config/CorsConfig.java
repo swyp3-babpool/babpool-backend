9e516708abaa8d58947f42c6173c91c3f26d4379
kang yeon joo <rkdduswndi@naver.com>
2024-03-06 11:49:19 +0900

    fix(user): return corsconfig

--- src/main/java/com/swyp3/babpool/global/config/CorsConfig.java
+++ src/main/java/com/swyp3/babpool/global/config/CorsConfig.java
@@ -14,15 +14,20 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${property.url.clientUrl}")
     private String clientUrl;
+    @Value("${property.url.clientUrlMain}")
+    private String clientUrlMain;
+    @Value("${property.url.clientUrlSub}")
+    private String clientUrlSub;
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        log.info("current clientUrl: {}, {}, {}", clientUrl);
+        log.info("current clientUrl: {}, {}, {}", clientUrl, clientUrlMain, clientUrlSub);
         registry
-            .addMapping("/**")
-            .allowedOrigins(clientUrl) // String... origins
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+                .addMapping("/**")
+                .allowedOrigins(clientUrl, clientUrlMain, clientUrlSub) // String... origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
