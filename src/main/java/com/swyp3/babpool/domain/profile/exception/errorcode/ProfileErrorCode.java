e16f62fa4f2c9dd45b0aa1e737902040043c16e2
kang yeon joo <rkdduswndi@naver.com>
2024-03-06 10:39:24 +0900

    feat(profile): Add error for getting profile

--- src/main/java/com/swyp3/babpool/domain/profile/exception/errorcode/ProfileErrorCode.java
+++ src/main/java/com/swyp3/babpool/domain/profile/exception/errorcode/ProfileErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ProfileErrorCode implements CustomErrorCode {
 
-    PROFILE_LIST_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "프로필 리스트 조회 중 오류가 발생했습니다.");
-
+    PROFILE_LIST_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "프로필 리스트 조회 중 오류가 발생했습니다."),
+    PROFILE_TARGET_PROFILE_ERROR(HttpStatus.BAD_REQUEST,"프로필 상세 조회 중 오류가 발생했습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
