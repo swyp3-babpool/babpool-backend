bd025c9b2458b1ef4a7e59bc628585861d8c68d7
proHyundo <guseh08@knou.ac.kr>
2024-03-07 13:55:21 +0900

    feat(review): Add reivew error codes

--- src/main/java/com/swyp3/babpool/domain/review/exception/ReviewErrorCode.java
+++ src/main/java/com/swyp3/babpool/domain/review/exception/ReviewErrorCode.java
@@ -9,6 +9,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ReviewErrorCode implements CustomErrorCode {
 
+    NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "There is no review for the requested profile."),
+    NOT_VALID_REVIEW_REQUEST(HttpStatus.BAD_REQUEST, "Not valid review request."),
+    REVIEW_CREATE_REQUEST_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to create review."),
+    REVIEW_UPDATE_REQUEST_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to update review."),
+    ALREADY_EXIST_REVIEW(HttpStatus.BAD_REQUEST, "Review already exists."),
+    REVIEW_LIST_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred while retrieving the review list."),
     ;
     private final HttpStatus httpStatus;
     private final String message;
