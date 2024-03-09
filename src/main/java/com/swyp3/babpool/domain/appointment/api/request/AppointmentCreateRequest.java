9d892930ed595e1d68e105f030166f5bb3fcb476
proHyundo <guseh08@knou.ac.kr>
2024-03-09 23:39:54 +0900

    fix(appointment): Resolve appointment

--- src/main/java/com/swyp3/babpool/domain/appointment/api/request/AppointmentCreateRequest.java
+++ src/main/java/com/swyp3/babpool/domain/appointment/api/request/AppointmentCreateRequest.java
@@ -8,26 +8,38 @@ import java.util.List;
 @Getter
 public class AppointmentCreateRequest {
 
-    private Long appointmentId;
-    private Long requesterUserId;
+    private Long appointmentId; // Not include in request body
+    private Long requesterUserId; // Not include in request body
+    private Long receiverUserId; // Not include in request body
     private Long targetProfileId;
     private List<Long> possibleTimeIdList;
     private String questionContents;
+    private Long appointmentRequestId; // Not include in request body
 
     @Builder
-    public AppointmentCreateRequest(Long appointmentId, Long requesterUserId, Long targetProfileId, List<Long> possibleTimeIdList, String questionContents) {
+    public AppointmentCreateRequest(Long appointmentId, Long requesterUserId, Long receiverUserId, Long targetProfileId, List<Long> possibleTimeIdList, String questionContents, Long appointmentRequestId) {
         this.appointmentId = appointmentId;
         this.requesterUserId = requesterUserId;
+        this.receiverUserId = receiverUserId;
         this.targetProfileId = targetProfileId;
         this.possibleTimeIdList = possibleTimeIdList;
         this.questionContents = questionContents;
+        this.appointmentRequestId = appointmentRequestId;
+    }
+
+    public void setAppointmentId(Long appointmentId) {
+        this.appointmentId = appointmentId;
     }
 
     public void setRequesterUserId(Long userId) {
         this.requesterUserId = userId;
     }
 
-    public void setTargetProfileId(Long targetProfileId) {
-        this.targetProfileId = targetProfileId;
+    public void setReceiverUserId(Long targetReceiverUserId) {
+        this.receiverUserId = targetReceiverUserId;
+    }
+
+    public void setAppointmentRequestId(Long appointmentRequestId) {
+        this.appointmentRequestId = appointmentRequestId;
     }
 }
