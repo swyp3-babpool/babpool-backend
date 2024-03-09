c5c6cdb467617f5ced4082e6d69e405c918441ac
proHyundo <guseh08@knou.ac.kr>
2024-03-09 18:41:01 +0900

    feat(schedul): Update babpool application add enablescheduling annotation

--- src/main/java/com/swyp3/babpool/BabpoolApplication.java
+++ src/main/java/com/swyp3/babpool/BabpoolApplication.java
@@ -2,7 +2,9 @@ package com.swyp3.babpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BabpoolApplication {
 
