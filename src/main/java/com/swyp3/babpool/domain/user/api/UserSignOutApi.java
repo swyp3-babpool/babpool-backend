746a4d0782f89cb7deef16050b04857e413e1fad
proHyundo <guseh08@knou.ac.kr>
2024-03-09 12:13:13 +0900

    refactor(signout): Update return maxage 0 refreshtoken cookie when sign out

--- src/main/java/com/swyp3/babpool/domain/user/api/UserSignOutApi.java
+++ src/main/java/com/swyp3/babpool/domain/user/api/UserSignOutApi.java
@@ -1,9 +1,11 @@
 package com.swyp3.babpool.domain.user.api;
 
+import com.swyp3.babpool.global.common.response.ApiResponseWithCookie;
 import com.swyp3.babpool.global.jwt.application.JwtServiceImpl;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,7 @@ public class UserSignOutApi {
     private final JwtServiceImpl jwtService;
 
     @PostMapping("/api/user/sign/out")
-    public void signOut(HttpServletRequest request){
+    public ApiResponseWithCookie signOut(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
         if(cookies != null){
             Optional<Cookie> optionalCookie = Arrays.stream(cookies)
@@ -25,6 +27,14 @@ public class UserSignOutApi {
                     .findAny();
             optionalCookie.ifPresent(cookie -> jwtService.logout(cookie.getValue()));
         }
+        return ApiResponseWithCookie.ofCookie(
+                HttpStatus.OK,
+                "sign out success",
+                null,
+                "refreshToken",
+                "",
+                true,
+                0);
     }
 
 }
