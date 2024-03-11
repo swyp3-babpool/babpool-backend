9abf39d3fed80b2a490cb457973bde68d83fe65b
proHyundo <guseh08@knou.ac.kr>
2024-03-11 09:12:46 +0900

    feat(kakao): Add kakao disconnect method and sign out method

--- src/main/java/com/swyp3/babpool/infra/auth/kakao/KakaoProvider.java
+++ src/main/java/com/swyp3/babpool/infra/auth/kakao/KakaoProvider.java
@@ -7,12 +7,19 @@ import com.swyp3.babpool.infra.auth.exception.errorcode.AuthExceptionErrorCode;
 import com.swyp3.babpool.infra.auth.response.AuthMemberResponse;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.security.PublicKey;
 import java.util.Map;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KakaoProvider {
@@ -23,6 +30,8 @@ public class KakaoProvider {
     private String iss;
     @Value("${property.oauth.kakao.client-id}")
     private String clientId;
+    @Value("${property.oauth.kakao.admin-key}")
+    private String KAKAO_SERVICE_APP_ADMIN_KEY;
 
     public AuthMemberResponse getKakaoPlatformMember(String identityToken){
         Map<String, String> headers = authJwtParser.parseHeaders(identityToken);
@@ -40,6 +49,38 @@ public class KakaoProvider {
             throw new AuthException(AuthExceptionErrorCode.AUTH_JWT_ERROR,
                     "OAuth Claim 값이 올바르지 않습니다.");
         }
+    }
+
+    public void kakaoMemberSignOut(Long userId) {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://kauth.kakao.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        JSONObject response = webClient.post()
+                .uri(uriBuilder -> uriBuilder.path("/v1/user/logout").build())
+                .header("Authorization", "KakaoAK " + clientId)
+                .retrieve().bodyToMono(JSONObject.class).block();
+
+        log.info("kakaoMemberSignOut response: " + response.toString());
+    }
+
+    public void kakaoMemberDisconnect(Long oauthId) {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://kapi.kakao.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        JSONObject response = webClient.post()
+                .uri(uriBuilder -> uriBuilder.path("/v1/user/unlink").build())
+                .header("Authorization", "KakaoAK " + KAKAO_SERVICE_APP_ADMIN_KEY)
+                .body(Mono.just(
+                        Map.of("target_id_type", "user_id",
+                                "target_id", oauthId)), JSONObject.class)
+                .retrieve().bodyToMono(JSONObject.class).block();
+
+        log.info("kakaoMemberDisconnect response: " + response.toString());
+
     }
 
 }
