f227e63895e11c7ddf91df278aa62948e2ffce55
proHyundo <guseh08@knou.ac.kr>
2024-03-11 09:12:02 +0900

    feat(signdown): Add social service disconnect method and get domain method

--- src/main/java/com/swyp3/babpool/infra/auth/service/AuthService.java
+++ src/main/java/com/swyp3/babpool/infra/auth/service/AuthService.java
@@ -1,6 +1,9 @@
 package com.swyp3.babpool.infra.auth.service;
 
+import com.swyp3.babpool.infra.auth.AuthPlatform;
 import com.swyp3.babpool.infra.auth.domain.Auth;
+import com.swyp3.babpool.infra.auth.exception.AuthException;
+import com.swyp3.babpool.infra.auth.exception.errorcode.AuthExceptionErrorCode;
 import com.swyp3.babpool.infra.auth.kakao.KakaoProvider;
 import com.swyp3.babpool.infra.auth.dao.AuthRepository;
 import com.swyp3.babpool.infra.auth.kakao.KakaoTokenProvider;
@@ -28,5 +31,38 @@ public class AuthService {
 
     public void save(Auth auth) {
         authRepository.save(auth);
+    }
+
+    public AuthPlatform getAuthPlatformByUserId(Long userId) {
+        return authRepository.findByUserId(userId).orElseThrow(
+                () -> new AuthException(AuthExceptionErrorCode.AUTH_INFO_NOT_FOUND,
+                        "해당 사용자의 소셜로그인 정보가 존재하지 않습니다.")
+        ).getOauthPlatformName();
+    }
+
+    public void socialServiceSignOut(Long userId, AuthPlatform authPlatform) {
+        switch (authPlatform) {
+            case KAKAO:
+                kakaoProvider.kakaoMemberSignOut(userId);
+                break;
+            default:
+                throw new AuthException(AuthExceptionErrorCode.NOT_SUPPORTED_AUTH_PLATFORM,
+                        "지원하지 않는 소셜로그인 플랫폼입니다.");
+        }
+    }
+
+    public void socialServiceDisconnect(Long userId, AuthPlatform authPlatform) {
+        switch (authPlatform) {
+            case KAKAO:
+                Auth auth = authRepository.findByUserId(userId).orElseThrow(
+                        () -> new AuthException(AuthExceptionErrorCode.AUTH_INFO_NOT_FOUND,
+                                "해당 사용자의 소셜로그인 정보가 존재하지 않습니다.")
+                );
+                kakaoProvider.kakaoMemberDisconnect(auth.getOauthId());
+                break;
+            default:
+                throw new AuthException(AuthExceptionErrorCode.NOT_SUPPORTED_AUTH_PLATFORM,
+                        "지원하지 않는 소셜로그인 플랫폼입니다.");
+        }
     }
 }
