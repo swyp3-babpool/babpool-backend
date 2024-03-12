31f33375eb8d0b784849d5942af2222dda06c999
proHyundo <guseh08@knou.ac.kr>
2024-03-13 00:01:12 +0900

    refactor(exception): Add deprecated annotation at BaseExceptionHandler

--- src/main/java/com/swyp3/babpool/global/common/exception/handler/BaseExceptionHandler.java
+++ src/main/java/com/swyp3/babpool/global/common/exception/handler/BaseExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+//@Deprecated
 //@Slf4j
 //@RestControllerAdvice
 //public class BaseExceptionHandler {
