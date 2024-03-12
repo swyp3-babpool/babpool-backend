19729ca6cdcd0eee1114af789dc5f7bbb5920541
proHyundo <guseh08@knou.ac.kr>
2024-03-12 20:29:11 +0900

    refactor(error): Update ApiErrorResponse use exist static method instead new operation

--- src/main/java/com/swyp3/babpool/global/common/response/ApiErrorResponse.java
+++ src/main/java/com/swyp3/babpool/global/common/response/ApiErrorResponse.java
@@ -1,7 +1,9 @@
 package com.swyp3.babpool.global.common.response;
 
 import com.swyp3.babpool.global.common.exception.errorcode.CustomErrorCode;
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -13,24 +15,25 @@ public class ApiErrorResponse {
     private final String statusDescription;
     private final String message;
 
+    @Builder
     public ApiErrorResponse(int statusCode, String statusDescription, String message) {
         this.statusCode = statusCode;
         this.statusDescription = statusDescription;
         this.message = message;
     }
 
-    public static ApiErrorResponse of(CustomErrorCode errorCode) {
-        return new ApiErrorResponse(
-                errorCode.getHttpStatus().value(),
-                errorCode.getHttpStatus().name(),
-                errorCode.getMessage()
-        );
+    public static ApiErrorResponse of(CustomErrorCode customErrorCode) {
+        return ApiErrorResponse.of(customErrorCode, customErrorCode.getMessage());
     }
 
-    public static ApiErrorResponse of(CustomErrorCode errorCode, String message) {
+    public static ApiErrorResponse of(CustomErrorCode customErrorCode, String message) {
+        return ApiErrorResponse.of(customErrorCode.getHttpStatus(), message);
+    }
+
+    public static ApiErrorResponse of(HttpStatus httpStatus, String message) {
         return new ApiErrorResponse(
-                errorCode.getHttpStatus().value(),
-                errorCode.getHttpStatus().name(),
+                httpStatus.value(),
+                httpStatus.name(),
                 message
         );
     }
