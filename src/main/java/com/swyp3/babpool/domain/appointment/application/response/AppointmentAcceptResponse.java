fff657153670ff043e6bfe429f32d74d5f33e257
kang yeon joo <rkdduswndi@naver.com>
2024-03-13 23:43:08 +0900

    fix: add AppointmentAcceptResponse data

--- src/main/java/com/swyp3/babpool/domain/appointment/application/response/AppointmentAcceptResponse.java
+++ src/main/java/com/swyp3/babpool/domain/appointment/application/response/AppointmentAcceptResponse.java
@@ -3,8 +3,17 @@ package com.swyp3.babpool.domain.appointment.application.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class AppointmentAcceptResponse {
-    private String okMessage;
+    private String requesterNickName;
+    private String requesterProfileImageUrl;
+    private String requesterGrade;
+    private String requesterIntro;
+    private LocalDateTime fixDateTime;
+    private String requesterContactPhone;
+    private String requesterContactChat;
+    private String question;
 }
