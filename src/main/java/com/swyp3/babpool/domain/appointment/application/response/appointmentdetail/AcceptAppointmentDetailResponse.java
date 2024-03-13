970e583ca9bde6cd7abed751588a1cd7c2d4248f
kang yeon joo <rkdduswndi@naver.com>
2024-03-13 22:46:33 +0900

    refactor: extract parent class AppointmentDetailResponse

---
+++ src/main/java/com/swyp3/babpool/domain/appointment/application/response/appointmentdetail/AcceptAppointmentDetailResponse.java
@@ -0,0 +1,20 @@
+package com.swyp3.babpool.domain.appointment.application.response.appointmentdetail;
+
+import com.swyp3.babpool.domain.appointment.application.response.AppointmentRequesterPossibleDateTimeResponse;
+import com.swyp3.babpool.domain.user.application.response.MyPageUserDto;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class AcceptAppointmentDetailResponse extends AppointmentDetailResponse{
+    private String contactChat;
+    private String contactPhone;
+
+    public AcceptAppointmentDetailResponse(MyPageUserDto userData, List<AppointmentRequesterPossibleDateTimeResponse> requesterPossibleTime, String question) {
+        super(userData,requesterPossibleTime,question);
+        this.contactChat= userData.getContactChat();
+        this.contactPhone=userData.getContactPhone();
+    }
+}
