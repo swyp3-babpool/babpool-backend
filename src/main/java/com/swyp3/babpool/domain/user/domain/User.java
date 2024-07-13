f01e28d48082d5b4fb817e8d309d97c15a5b2487
proHyundo <guseh08@knou.ac.kr>
2024-07-13 10:45:07 +0900

    feat(user): User 도메인 클래스에 모든 인자 Builder 추가

--- src/main/java/com/swyp3/babpool/domain/user/domain/User.java
+++ src/main/java/com/swyp3/babpool/domain/user/domain/User.java
@@ -4,9 +4,11 @@ import com.swyp3.babpool.infra.auth.response.AuthMemberResponse;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@ToString
 @Getter
 @NoArgsConstructor
 public class User {
@@ -29,5 +31,17 @@ public class User {
         this.userNickName = nickName;
         this.userCreateDate = LocalDateTime.now();
         this.userModifyDate = LocalDateTime.now();
+    }
+
+    @Builder(builderMethodName = "allArgsBuilder", buildMethodName = "allArgsBuild", builderClassName = "allArgsBuilder")
+    public User(Long userId, String userEmail, UserStatus userStatus, UserRole userRole, String userGrade, String userNickName, LocalDateTime userCreateDate, LocalDateTime userModifyDate) {
+        this.userId = userId;
+        this.userEmail = userEmail;
+        this.userStatus = userStatus;
+        this.userRole = userRole;
+        this.userGrade = userGrade;
+        this.userNickName = userNickName;
+        this.userCreateDate = userCreateDate;
+        this.userModifyDate = userModifyDate;
     }
 }
