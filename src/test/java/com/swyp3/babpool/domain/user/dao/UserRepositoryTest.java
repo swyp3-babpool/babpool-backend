89a2ede240f0d21f85b8d2aa82ba33b9572af6a6
proHyundo <guseh08@knou.ac.kr>
2024-07-14 15:12:06 +0900

    test(user): UserRepository 쿼리 테스트 클래스 추가

--- src/test/java/com/swyp3/babpool/domain/user/dao/UserRepositoryTest.java
+++ src/test/java/com/swyp3/babpool/domain/user/dao/UserRepositoryTest.java
@@ -91,4 +91,99 @@ class UserRepositoryTest {
         Assertions.assertThat(user.getUserId()).isEqualTo(100000000000000001L);
         Assertions.assertThat(user.getUserGrade()).isEqualTo("FIRST");
     }
+
+    @DisplayName("updateSignUpInfo 매퍼는 특정 사용자의 학력을 수정한다.")
+    @Test
+    void updateSignUpInfo(){
+        // given
+        Long userId = 100000000000000001L;
+        String userGrade = "SECOND";
+
+        // when
+        userRepository.updateSignUpInfo(userId, userGrade);
+
+        // then
+        String updatedUserGrade = userRepository.findUserGradeById(userId);
+        Assertions.assertThat(updatedUserGrade).isEqualTo("SECOND");
+    }
+
+    @DisplayName("findUserGradeById 매퍼는 특정 사용자의 학력을 조회한다.")
+    @Test
+    void findUserGradeById(){
+        // given
+        Long userId = 100000000000000001L;
+
+        // when
+        String userGrade = userRepository.findUserGradeById(userId);
+
+        // then
+        Assertions.assertThat(userGrade).isEqualTo("FIRST");
+    }
+
+    @DisplayName("updateUserStateByUserId 매퍼는 특정 사용자의 상태를 수정한다.")
+    @Test
+    void updateUserStateByUserId(){
+        // given
+        Long userId = 100000000000000001L;
+        UserStatus userStatus = UserStatus.EXIT;
+
+        // when
+        userRepository.updateUserStateByUserId(userId, userStatus);
+
+        // then
+        User user = userRepository.findById(userId);
+        Assertions.assertThat(user.getUserStatus()).isEqualTo(UserStatus.EXIT);
+    }
+
+    @DisplayName("findActiveUserByUserEmail 매퍼는 특정 사용자의 이메일로 ACTIVE 상태의 사용자를 조회한다.")
+    @Test
+    void findActiveUserByUserEmail() {
+        // given
+        String targetUserEmail = "test1@test.com";
+
+        // when
+        User user = userRepository.findActiveUserByUserEmail(targetUserEmail);
+
+        // then
+        Assertions.assertThat(user.getUserStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @DisplayName("findActiveUserByUserEmail 매퍼는 ACTIVE 상태가 아닌 사용자는 조회하지 않는다.")
+    @Test
+    void findActiveUserByUserEmail_notActive() {
+        // given
+        User targetUser = User.allArgsBuilder()
+                .userId(tsidKeyGenerator.generateTsid())
+                .userEmail("temp@test.com")
+                .userStatus(UserStatus.PREACTIVE)
+                .userRole(UserRole.USER)
+                .userGrade("FIRST")
+                .userNickName("temp")
+                .allArgsBuild();
+        userRepository.save(targetUser);
+
+        // when
+        User user = userRepository.findActiveUserByUserEmail(targetUser.getUserEmail());
+
+        // then
+        assertNull(user);
+    }
+
+    @DisplayName("updateUserNickNameAndGrade 매퍼는 특정 사용자의 닉네임과 학력을 수정한다.")
+    @Test
+    void updateUserNickNameAndGrade(){
+        // given
+        Long userId = 100000000000000001L;
+        String userNickName = "newNickName";
+        String userGrade = "SECOND";
+
+        // when
+        userRepository.updateUserNickNameAndGrade(userId, userNickName, userGrade);
+
+        // then
+        User user = userRepository.findById(userId);
+        Assertions.assertThat(user.getUserNickName()).isEqualTo("newNickName");
+        Assertions.assertThat(user.getUserGrade()).isEqualTo("SECOND");
+    }
+
 }
