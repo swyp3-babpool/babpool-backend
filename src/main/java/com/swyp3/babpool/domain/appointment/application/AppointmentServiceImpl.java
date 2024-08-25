23ea7fedbde1710c2fa1b92ff8a44c0952956e8a
proHyundo <guseh08@knou.ac.kr>
2024-08-25 13:31:34 +0900

    update(appointment): 밥 약속 신청 시 날짜및시간이 올바른 형태, 범위인지 검증

--- src/main/java/com/swyp3/babpool/domain/appointment/application/AppointmentServiceImpl.java
+++ src/main/java/com/swyp3/babpool/domain/appointment/application/AppointmentServiceImpl.java
@@ -41,8 +41,6 @@ public class AppointmentServiceImpl implements AppointmentService{
 
     private final PossibleDateTimeService possibleDateTimeService;
     private final RejectService rejectService;
-    private final UserService userService;
-    private final ProfileService profileService;
 
     private final AppointmentRepository appointmentRepository;
     private final UserRepository userRepository;
@@ -57,6 +55,9 @@ public class AppointmentServiceImpl implements AppointmentService{
     @Transactional
     @Override
     public AppointmentCreateResponse makeAppointmentResolveConcurrency(AppointmentCreateRequest appointmentCreateRequest) {
+
+        // 요청한 시간이 기획 요구사항과 일치하는지 확인.
+        validateDateTimeRange(appointmentCreateRequest.getPossibleDateTime());
 
         // 요청 송신자와 수신자 동일한 요청일 경우 예외를 발생한다.
         throwExceptionIfSenderAndReceiverAreSame(appointmentCreateRequest);
@@ -88,6 +89,23 @@ public class AppointmentServiceImpl implements AppointmentService{
         simpleMessagingPublisher.sendAppointmentRequestMessageToAppointmentReceiver(savedAppointment.getAppointmentId(), savedAppointment.getAppointmentSenderId(), savedAppointment.getAppointmentReceiverId());
 
         return AppointmentCreateResponse.of(savedAppointment, appointmentCreateRequest.getTargetProfileId());
+    }
+
+    private void validateDateTimeRange(LocalDateTime possibleDateTime) {
+        LocalDateTime curDateTime = LocalDateTime.now();
+        if(possibleDateTime.isBefore(curDateTime)){
+            throw new AppointmentException(AppointmentErrorCode.APPOINTMENT_POSSIBLE_DATETIME_INVALID,
+                    "밥약 가능한 시간이 현재 시간 이전입니다.");
+        }
+
+        if(possibleDateTime.getHour() < 8 || possibleDateTime.getHour() > 22){
+            throw new AppointmentException(AppointmentErrorCode.APPOINTMENT_POSSIBLE_DATETIME_INVALID,
+                    "밥약 가능한 시간이 08시부터 22시 사이가 아닙니다.");
+        }
+        if(possibleDateTime.getMinute() != 0 || possibleDateTime.getSecond() != 0){
+            throw new AppointmentException(AppointmentErrorCode.APPOINTMENT_POSSIBLE_DATETIME_INVALID,
+                    "밥약 가능한 시간이 '00분 00초' 가 아닙니다.");
+        }
     }
 
     private static void throwExceptionIfSenderAndReceiverAreSame(AppointmentCreateRequest appointmentCreateRequest) {
